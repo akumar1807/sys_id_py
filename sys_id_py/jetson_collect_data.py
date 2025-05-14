048b8411cad429bef1c8a6375d19e7ca1edc748b
AyushKumar <ayush18.kumar@gmail.com>
2025-05-14 15:17:54 +0530

    Removed log info

--- sys_id_py/jetson_collect_data.py
+++ sys_id_py/jetson_collect_data.py
@@ -80,12 +80,12 @@ class JetsonDataLogger(Node):
     
     def loop(self):
         while rclpy.ok():
-            #self.get_logger().info("Collecting Data")
             self.collect_data()
             print(self.counter)
             if self.counter == self.timesteps + 1:
                 self.export_data_as_csv()
                 self.file.close()
+                self.destroy_node()
                 rclpy.shutdown()
 
 def main(args=None):
